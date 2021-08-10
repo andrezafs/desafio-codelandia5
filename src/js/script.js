35c4aaa1fe3fcf7035f58566b8f721cb9cc57d50
Andreza Ferreira <andrezafs.rr@gmail.com>
2021-08-09 21:42:47 -0300

    adição de animações

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,19 @@ const bg = ScrollReveal({
   reset: true,
 });
 bg.reveal("#bg", { delay: 20 });
+
+const texts = ScrollReveal({
+  origin: "right",
+  distance: "50px",
+  duration: 6000,
+  reset: true,
+});
+texts.reveal("#texts", { delay: 50 });
+
+const header = ScrollReveal({
+  origin: "right",
+  distance: "50px",
+  duration: 6000,
+  reset: true,
+});
+header.reveal("header", { delay: 50 });
